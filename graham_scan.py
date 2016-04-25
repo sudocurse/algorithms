36875bfd382b8aac3633534939a45d63cb78689f
Ankeet <postmillenial@gmail.com>
2016-04-25 04:19:47 -0400

    added most of the math
    - replaces placeholder functions with crossproduct and quicksort
    - renames variables to be more descriptive, has function
    nondestructively deal with arrays that get used later
    -removing the range() in the for loop for a cleaner list enumeration.
    don’t need the index at all!

--- graham_scan.py
+++ graham_scan.py
@@ -10,12 +10,35 @@ def find_min_y(input_q):
     '''returns smallest y in set. if tie, goes by smallest x.'''
     return input_q.index(min(input_q, key=itemgetter(1, 0)))
 
-def polar_sorted(input_q, min_y):
-    '''sorts list by polar angle counterclockwise around p0 (the min_y)'''
+def cross_product(vector1, vector2):
+    '''returns cross product of two vectors (x1 * y2) - (x2 * y1)'''
+
+    #if this is positive, v1 is clockwise from p2; if negative, counterclockwise
+    #if this is zero, the vectors are equivalent or collinear, or one is len 0
+    return (vector1[0] * vector2[1]) - (vector2[0] * vector1[1])
+
+    # so in sorted order a,b, a is clockwise from b cross product
+    # meaning would be like a x b > 0
+
+def polar_sort(input_q, orig_pt):
+    #TODO: testing for assumptions == verifying cross product and sort order
+    '''sorts list by polar angle counterclockwise around p0 (the orig_pt)'''
     #TODO implement polar_sorted. can this be parallelized
-    if min_y is None:
-        print "this hsouldn't happen"
-    return input_q
+    if orig_pt is None:
+        print "this shouldn't happen"
+
+    normalized = map(lambda x: (x[0] - orig_pt[0], x[1] - orig_pt[1]), input_q)
+
+
+    def qsort(L):
+        '''a cross product quicksort. if thing x pt >0, counterclockwise,
+            if thing x point > 0, clockwise. and i thiIink it discards colinear?
+        '''
+        if len(L) <= 1: return L
+        return qsort([i for i in L[1:] if cross_product(i,L[0])>0]) + L[0:1] + \
+               qsort([i for i in L[1:] if cross_product(i,L[0])<0])
+
+    return map(lambda x: (x[0] + orig_pt[0], x[1] + orig_pt[1]), qsort(normalized))
 
 def angle(top, next_top, min_y):
     """
@@ -23,47 +46,45 @@ def angle(top, next_top, min_y):
     """
     if top is None or next_top is None or min_y is None:
         print "this shouldn't happen"
-    #TODO angle. can this be parallelized
-    #pass
+
+    #TODO implement this! should be the last piece
 
 def graham_scan(input_set):
     '''
         Takes a set of points of size >= 3 and returns smallest bounding polygon.
         Size assumption is tested for.
     '''
-    #TODO Assumes points are unique, and not collinear.
 
     assert len(input_set) >= 3 # q
     # assert unique
     # assert non-collinear
 
-    candidates = [] # p
-    results = [] # s
+    polar_sorted = [] # p
+    vertices = [] # s
 
     mindex = find_min_y(input_set)
-    candidates.append(input_set[mindex])
-    del input_set[mindex] # why does this keyword feel gross
+    polar_sorted.append(input_set[mindex])
+    input_set = input_set[:mindex] + input_set[mindex+1:] # why does this keyword feel gross
 
-    candidates.extend(polar_sorted(input_set, candidates[0]))
+    polar_sorted.extend(polar_sort(input_set, polar_sorted[0]))
+    # so far so good....
 
-    results.append(candidates[0])
-    results.append(candidates[1])
-    results.append(candidates[2])
+    print "sorted: ", polar_sorted
 
-    for i in range(3, len(candidates)):
-        # enumerate for index?? may need to pass that info onto angle()
-        while angle(results[-1], results[-2], candidates[i]) > 90: # degrees
-            results.pop()
+    vertices.append(polar_sorted[0])
+    vertices.append(polar_sorted[1])
+    vertices.append(polar_sorted[2])
 
-        results.append(candidates[1])
+    print "vertices: ", vertices
 
-    return results
+    for candidate_point in polar_sorted[3:]:
+        while angle(vertices[-1], vertices[-2], candidate_point) > 90: # degrees
+            vertices.pop()
+        vertices.append(candidate_point)
 
-def generate_coordinates(length):
-    '''input number of tuples. return that many rand tuples.'''
-    import random
-    rsamp = random.sample(range(1, 100), length * 2)
-    return zip(rsamp[:length], rsamp[length:])
+    return vertices
+
+
 
 def plot(dset, bound):
     '''plot the list of tuples dset and create a bounding polygon from bound'''
@@ -78,11 +99,16 @@ def plot(dset, bound):
     pyplot.scatter(*zip(*dset))
     pyplot.show()
 
-if __name__ == '__main__':
-    #
-    #TODO: write some unit tests to make sure graham_scan(data) fails on
-    #       bad length, non-unique, or collinear input data
+def generate_coordinates(length):
+    '''input number of tuples. return that many rand tuples.'''
+    import random
+    rsamp = random.sample(range(1, 100), length * 2)
+    return zip(rsamp[:length], rsamp[length:])
 
-    data = generate_coordinates(5)
+if __name__ == '__main__':
+
+    data = generate_coordinates(4)
+    print "data =", data
     boundaries = graham_scan(data)
+    print "bounds ",boundaries
     plot(data, boundaries)
